e0bd2d70df51f077c256f2b7bfe9cc71b5cc2ee5
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-10-12 22:33:22 -0400

    Working at least a bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
 
-function App() {
+const App: React.VFC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <KVStore />
       </header>
     </div>
   );
-}
+};
+
+const KVStore: React.VFC = () => {
+  const [kvs, setKvs] = useState<Record<string, string>>({});
+  const genRandomKv = () => {
+    const k = Math.random().toString(36).substring(2);
+    const v = Math.random().toString(36).substring(2);
+    setKvs((cur) => ({ ...cur, [k]: v }));
+  };
+  return (
+    <KVTable>
+      <KVHeader>
+        <td>Key</td>
+        <td>Value</td>
+      </KVHeader>
+      {Object.entries(kvs).map((k, v) => (
+        <tr>
+          <td>{k}</td>
+          <td>{v}</td>
+        </tr>
+      ))}
+      <tr>
+        <button onClick={() => genRandomKv()}>Click me!</button>
+      </tr>
+    </KVTable>
+  );
+};
+
+const KVTable = styled.table`
+  border: 1px solid black;
+  border-collapse: collapse;
+`;
+
+const KVHeader = styled.tr`
+  border-bottom: 1px solid black;
+`;
 
 export default App;
