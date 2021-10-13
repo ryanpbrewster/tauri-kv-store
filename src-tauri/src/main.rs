1323e5a9160e1253262d21b00a9624dfe1b6119f
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-10-13 00:18:11 -0400

    Use a connection pool

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,22 @@
   windows_subsystem = "windows"
 )]
 
+use r2d2_sqlite::SqliteConnectionManager;
+
 fn main() {
+  let mut path = dirs::cache_dir().unwrap();
+  path.push("tauri-app.sql");
+  let manager = r2d2_sqlite::SqliteConnectionManager::file(path);
+  let pool = r2d2::Pool::new(manager).unwrap();
+  let conn = pool.get().unwrap();
+  conn
+    .execute(
+      "CREATE TABLE IF NOT EXISTS kvs (k TEXT PRIMARY KEY, v TEXT NOT NULL)",
+      [],
+    )
+    .unwrap();
   tauri::Builder::default()
+    .manage(pool)
     .invoke_handler(tauri::generate_handler![
       my_custom_command,
       persist_entry,
@@ -20,17 +34,9 @@ fn my_custom_command(name: String) -> String {
 }
 
 #[tauri::command]
-fn fetch_entries() -> Vec<(String, String)> {
+fn fetch_entries(pool: tauri::State<r2d2::Pool<SqliteConnectionManager>>) -> Vec<(String, String)> {
   println!("fetching all entries");
-  let mut path = dirs::cache_dir().unwrap();
-  path.push("tauri-app.sql");
-  let conn = rusqlite::Connection::open(path).unwrap();
-  conn
-    .execute(
-      "CREATE TABLE IF NOT EXISTS kvs (k TEXT PRIMARY KEY, v TEXT NOT NULL)",
-      [],
-    )
-    .unwrap();
+  let conn = pool.get().unwrap();
   let mut stmt = conn.prepare("SELECT k, v FROM kvs").unwrap();
   stmt
     .query_map([], |row| Ok((row.get_unwrap("k"), row.get_unwrap("v"))))
@@ -40,11 +46,13 @@ fn fetch_entries() -> Vec<(String, String)> {
 }
 
 #[tauri::command]
-fn persist_entry(key: String, value: String) {
+fn persist_entry(
+  pool: tauri::State<r2d2::Pool<SqliteConnectionManager>>,
+  key: String,
+  value: String,
+) {
   println!("recording {} = {}", key, value);
-  let mut path = dirs::cache_dir().unwrap();
-  path.push("tauri-app.sql");
-  let conn = rusqlite::Connection::open(path).unwrap();
+  let conn = pool.get().unwrap();
   conn
     .execute(
       "CREATE TABLE IF NOT EXISTS kvs (k TEXT PRIMARY KEY, v TEXT NOT NULL)",
